cad214fb63457dcb89813ada53e107594dc6e4cb
Almorelo <72256934+Almorelo@users.noreply.github.com>
2021-10-12 23:12:37 +0200

    v4

--- core/settings.py
+++ core/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django-app-db',
+        'NAME': 'djangoappdb',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'db',
