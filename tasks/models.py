093f2c548a4d8186d878be614be6d4e187e010bf
Almorelo <72256934+Almorelo@users.noreply.github.com>
2021-10-13 14:59:08 +0200

    v5

--- tasks/models.py
+++ tasks/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=250)
     complete = models.BooleanField(default=False)
-    # description = models.CharField(max_length=100)
+    description = models.CharField(max_length=110, default="")
     name = models.CharField(max_length=145, default="")
+    surname = models.CharField(max_length=145, default="")
 
     def __str__(self):
         return self.title
